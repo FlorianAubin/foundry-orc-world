562b306d157285a40b12ad6ca935c7cebef273e8
pi <remi.ratajczak@gmail.com>
2023-04-26 18:53:54 +0200

    hotfix: fetch regularly

--- scripts/auto_pull/auto_pull.sh
+++ scripts/auto_pull/auto_pull.sh
@@ -7,6 +7,7 @@ remote_commit=$(git -C $ORCS_WORLD_PATH rev-parse origin/main)
 local_commit=$(git -C $ORCS_WORLD_PATH rev-parse main)
 
 while true; do
+	git fetch
 
 	if [ "$remote_commit" != "$local_commit" ]; then
 		echo "Checkout main"
